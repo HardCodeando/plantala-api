68c1483c8073c8d75a7a3a8ceab85a6f9a9c56aa
Jose Aparcana <josered30@hotmail.com>
2022-05-05 22:02:13 -0500

    migration script

--- src/plants/api/controllers/plants.controller.ts
+++ src/plants/api/controllers/plants.controller.ts
@@ -3,7 +3,7 @@ import { ApiTags } from '@nestjs/swagger';
 import { GetPlantsInteractor } from '../../interactor/get-plants';
 
 @ApiTags('Plants')
-@Controller()
+@Controller('plants')
 export class PlantsController {
   constructor(private readonly getPlansInteractor: GetPlantsInteractor) {}
 
